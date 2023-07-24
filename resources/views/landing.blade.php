3d4ed35979c6a29ede115a5dc73aeb9f54e44e69
FadilPratama <fadilpratama.business@gmail.com>
2023-07-24 12:15:59 +0700

    Update landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -164,7 +164,7 @@
 
                 <div class="section-title">
                     <h2>Tentang Kami</h2>
-                    <p>Jaga Jagung Giriroto adalah sebuah website yang didedikasikan untuk para petani jagung dengan
+                    <p>Jaga Jagung Giriroto adalah sebuah platform yang didedikasikan untuk para petani jagung dengan
                         tujuan membantu mereka dalam mengelola dan menjaga tanaman jagung mereka. Melalui platform ini,
                         para petani dapat memantau pertumbuhan tanaman, mengidentifikasi penyakit dan hama yang mungkin
                         mempengaruhi tanaman jagung, serta mendapatkan rekomendasi perawatan yang tepat. Website ini
@@ -371,27 +371,27 @@
 
         <div class="container d-md-flex py-4">
 
-      <div class="me-md-auto text-center text-md-start">
-        <div class="copyright">
-          &copy; Copyright <strong><span>Dr.brokoli</span></strong>. All Rights Reserved
+            <div class="me-md-auto text-center text-md-start">
+                <div class="copyright">
+                    &copy; Copyright <strong><span>Dr.brokoli</span></strong>. All Rights Reserved
+                </div>
+                <div class="credits">
+                    <!-- All the links in the footer should remain intact. -->
+                    <!-- You can delete the links only if you purchased the pro version. -->
+                    <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/ -->
+                    Designed by <a href="https://bootstrapmade.com/">Kelompok 12</a>
+                </div>
+            </div>
+            <div class="social-links text-center text-md-right pt-3 pt-md-0">
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
         </div>
-        <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/ -->
-          Designed by <a href="https://bootstrapmade.com/">Kelompok 12</a>
-        </div>
-      </div>
-      <div class="social-links text-center text-md-right pt-3 pt-md-0">
-        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-      </div>
-    </div>
-  </footer><!-- End Footer -->
+    </footer><!-- End Footer -->
 
     <div id="preloader"></div>
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
