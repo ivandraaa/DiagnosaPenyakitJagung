2f6786ff1684c3c5edc30ede2d95fb510371d8e5
Fawwaz Ivandra <ivandrafawwaz@gmail.com>
2023-07-26 00:26:29 +0700

    ubah nilai keputusan pakar

--- app/Models/Keputusan.php
+++ app/Models/Keputusan.php
@@ -30,62 +30,62 @@ class Keputusan extends Model
             [
                 'kode_penyakit' => 'P001',
                 'kode_gejala' => 'G001',
-                'mb' => 0.6,
-                'md' => 0.4
+                'mb' => 0.8,
+                'md' => 0.2
             ],
             [
                 'kode_penyakit' => 'P001',
                 'kode_gejala' => 'G002',
-                'mb' => 0.8,
-                'md' => 0.2
+                'mb' => 0.6,
+                'md' => 0.4
             ],
             [
                 'kode_penyakit' => 'P001',
                 'kode_gejala' => 'G003',
-                'mb' => 0.6,
-                'md' => 0.4
+                'mb' => 0.8,
+                'md' => 0.2
             ],
             [
                 'kode_penyakit' => 'P001',
                 'kode_gejala' => 'G004',
-                'mb' => 0.6,
-                'md' => 0.4
+                'mb' => 0.2,
+                'md' => 0.8
             ],
             [
                 'kode_penyakit' => 'P001',
                 'kode_gejala' => 'G005',
-                'mb' => 0.6,
-                'md' => 0.4
+                'mb' => 0.8,
+                'md' => 0.2
             ],
             [
                 'kode_penyakit' => 'P002',
                 'kode_gejala' => 'G006',
-                'mb' => 0.6,
-                'md' => 0.4
+                'mb' => 0.2,
+                'md' => 0.8
             ],
             [
                 'kode_penyakit' => 'P002',
                 'kode_gejala' => 'G007',
-                'mb' => 0.8,
-                'md' => 0.2
-            ],
-            [
-                'kode_penyakit' => 'P002',
-                'kode_gejala' => 'G008',
                 'mb' => 0.6,
                 'md' => 0.4
             ],
             [
                 'kode_penyakit' => 'P002',
-                'kode_gejala' => 'G009',
+                'kode_gejala' => 'G008',
                 'mb' => 0.8,
                 'md' => 0.2
             ],
             [
                 'kode_penyakit' => 'P002',
+                'kode_gejala' => 'G009',
+                'mb' => 0.6,
+                'md' => 0.4
+            ],
+            [
+                'kode_penyakit' => 'P002',
                 'kode_gejala' => 'G010',
-                'mb' => 1,
-                'md' => 0
+                'mb' => 0.8,
+                'md' => 0.2
             ],
             [
                 'kode_penyakit' => 'P003',
@@ -96,8 +96,8 @@ class Keputusan extends Model
             [
                 'kode_penyakit' => 'P003',
                 'kode_gejala' => 'G012',
-                'mb' => 0.6,
-                'md' => 0.4
+                'mb' => 1,
+                'md' => 0
             ],
             [
                 'kode_penyakit' => 'P003',
@@ -108,20 +108,20 @@ class Keputusan extends Model
             [
                 'kode_penyakit' => 'P003',
                 'kode_gejala' => 'G014',
-                'mb' => 0.6,
-                'md' => 0.4
-            ],
-            [
-                'kode_penyakit' => 'P003',
-                'kode_gejala' => 'G015',
                 'mb' => 0.8,
                 'md' => 0.2
             ],
             [
+                'kode_penyakit' => 'P003',
+                'kode_gejala' => 'G015',
+                'mb' => 0.2,
+                'md' => 0.8
+            ],
+            [
                 'kode_penyakit' => 'P004',
                 'kode_gejala' => 'G016',
-                'mb' => 0.6,
-                'md' => 0.4
+                'mb' => 0.8,
+                'md' => 0.2
             ],
             [
                 'kode_penyakit' => 'P004',
@@ -132,8 +132,8 @@ class Keputusan extends Model
             [
                 'kode_penyakit' => 'P004',
                 'kode_gejala' => 'G018',
-                'mb' => 0.8,
-                'md' => 0.2
+                'mb' => 0.6,
+                'md' => 0.4
             ],
             [
                 'kode_penyakit' => 'P004',
@@ -144,26 +144,26 @@ class Keputusan extends Model
             [
                 'kode_penyakit' => 'P004',
                 'kode_gejala' => 'G020',
-                'mb' => 0.6,
-                'md' => 0.4
-            ],
-            [
-                'kode_penyakit' => 'P005',
-                'kode_gejala' => 'G021',
-                'mb' => 0.6,
-                'md' => 0.4
-            ],
-            [
-                'kode_penyakit' => 'P005',
-                'kode_gejala' => 'G022',
                 'mb' => 0.8,
                 'md' => 0.2
             ],
             [
                 'kode_penyakit' => 'P005',
+                'kode_gejala' => 'G021',
+                'mb' => 0.8,
+                'md' => 0.2
+            ],
+            [
+                'kode_penyakit' => 'P005',
+                'kode_gejala' => 'G022',
+                'mb' => 0.6,
+                'md' => 0.4
+            ],
+            [
+                'kode_penyakit' => 'P005',
                 'kode_gejala' => 'G023',
-                'mb' => 1,
-                'md' => 0
+                'mb' => 0.2,
+                'md' => 0.8
             ],
             [
                 'kode_penyakit' => 'P005',
@@ -174,8 +174,8 @@ class Keputusan extends Model
             [
                 'kode_penyakit' => 'P005',
                 'kode_gejala' => 'G025',
-                'mb' => 0.6,
-                'md' => 0.4
+                'mb' => 0.8,
+                'md' => 0.2
             ],
         ];
         return $rule;
